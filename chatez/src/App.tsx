1dd8866ac85e2112887eb7a14bffea1279ccdf58
Abdul Rehman <87334764+X2Abdul@users.noreply.github.com>
2024-10-17 18:11:38 +0100

    added react router dom and an example

--- chatez/src/App.tsx
+++ chatez/src/App.tsx
@@ -2,15 +2,44 @@ import logo from './media/logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+// Components for the different pages to test react router
+const Home = () => <h2>Home Page</h2>;
+const About = () => <h2>About Page</h2>;
+const Contact = () => <h2>Contact Page</h2>;
 
 function App() {
   return (
-    <div className="App">
-      <h1>ChatEz</h1>
-      <img src={logo} alt="chatEz" />
-      <br />
-      <Button variant="outline-primary">Button</Button>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>ChatEz</h1>
+        <img src={logo} alt="chatEz" />
+        <br />
+        <Button variant="outline-primary">Button</Button>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </Router>
+      </div>
+    </Router>
   );
 }
 

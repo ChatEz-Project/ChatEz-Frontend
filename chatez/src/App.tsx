f5bc6f966cc31865bfbc5f1bb6297c7e4b8fcd34
Abdul Rehman <87334764+X2Abdul@users.noreply.github.com>
2024-10-17 18:02:33 +0100

    added bootstrap and an example

--- chatez/src/App.tsx
+++ chatez/src/App.tsx
@@ -1,11 +1,15 @@
 import logo from './media/logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <h1>ChatEz</h1>
       <img src={logo} alt="chatEz" />
+      <br />
+      <Button variant="outline-primary">Button</Button>
     </div>
   );
 }

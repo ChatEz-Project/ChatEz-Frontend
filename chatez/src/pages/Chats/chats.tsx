21e4d8d60a02862a31978e8268c8f37712abed8b
Jamie-Lee Gordon <gordonjamie123@yahoo.co.uk>
2024-10-31 19:26:08 +0000

    text to speech

--- chatez/src/pages/Chats/chats.tsx
+++ chatez/src/pages/Chats/chats.tsx
@@ -3,16 +3,12 @@ import {
   AppBar,
   Toolbar,
   Typography,
-  Container,
   Box,
   Button,
-  Card,
-  CardContent,
-  Grid,
 } from '@mui/material';
 import { Home as HomeIcon } from '@mui/icons-material';
 import { doSignOut } from '../../firebase/auth';
-import { useHistory } from 'react-router-dom';
+import TextToSpeechAPI from '../../globalHooks/useTextToSpeech';
 
 const HomePage: React.FC = () => {
   const handleSignOut = () => {
@@ -32,34 +28,8 @@ const HomePage: React.FC = () => {
         </Toolbar>
       </AppBar>
 
-      <Container maxWidth="lg" sx={{ mt: 4 }}>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Welcome to My App
-          </Typography>
-          <Typography variant="h6" component="h2" gutterBottom>
-            Your one-stop solution for everything
-          </Typography>
-        </Box>
-
-        <Grid container spacing={3}>
-          {[1, 2, 3].map((item) => (
-            <Grid item xs={12} sm={4} key={item}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    Feature {item}
-                  </Typography>
-                  <Typography variant="body2">
-                    This is a brief description of Feature {item}. Replace this
-                    with actual content about your app's features.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+      
+      <TextToSpeechAPI />
     </Box>
   );
 };

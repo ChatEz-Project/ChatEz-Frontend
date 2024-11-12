2d0cec565f3fb075b469551bd33be09ad054ea5f
Jamie-Lee Gordon <gordonjamie123@yahoo.co.uk>
2024-11-12 20:31:35 +0000

    Profile Panel UI
    Profile Panel UI completed

--- chatez/src/pages/Chats/Components/ChatFeedProfilePanel/ChatFeedProfilePanel.tsx
+++ chatez/src/pages/Chats/Components/ChatFeedProfilePanel/ChatFeedProfilePanel.tsx
@@ -1,11 +1,15 @@
 import { AccountCircle } from "@mui/icons-material";
 import "./ChatFeedProfilePanel.css";
+import { useEffect, useState } from "react";
 
 interface ChatFeedProfilePanelProps {
     className?: string;
 }
 
 const ChatFeedProfilePanel: React.FC<ChatFeedProfilePanelProps> = ({ className }) => {
+
+    const [sharedImages, setSharedImages] = useState<string[]>([])
+    const [sharedFiles, setSharedFiles] = useState<string[]>([])
 
     const deleteConversation = () => {
         console.log("Conversation has been deleted")
@@ -15,35 +19,45 @@ const ChatFeedProfilePanel: React.FC<ChatFeedProfilePanelProps> = ({ className }
         console.log("Friend has been removed")
     }
 
+    useEffect(() => {
+        setSharedImages([
+            "https://i.pinimg.com/736x/3d/cd/4a/3dcd4af5bc9e06d36305984730ab7888.jpg",
+            "https://storage.googleapis.com/chatez-438923.firebasestorage.app/MessageFiles%2F56rolsj%40gmail.com-1731362731422-phoenix_cropped.png",
+            "https://storage.googleapis.com/chatez-438923.firebasestorage.app/ProfilePhotos%2F56rolsj%40gmail.com-1731363038271-tenor.gif"
+        ])
+        setSharedFiles([
+            "DocumentExample1.docx",
+            "DocumentExample2.docx"
+        ])
+    }, []);
+
     return (
         <div className = {className ? `ChatFeedProfilePanel ${className}` : "ChatFeedProfilePanel"}>
             <AccountCircle id = "Profile-picture" />
             <h1 id = "Display-name">Abdul</h1>
             <hr />
-            <h2>Shared Photos</h2>
 
-            <img 
-                className="SharedPhoto-image"
-                src = "https://i.pinimg.com/736x/3d/cd/4a/3dcd4af5bc9e06d36305984730ab7888.jpg"
-                alt = "Shared Media"
-            />
+            <h2>Shared Images</h2>
 
-            <img 
-                className="SharedPhoto-image"
-                src = "https://storage.googleapis.com/chatez-438923.firebasestorage.app/MessageFiles%2F56rolsj%40gmail.com-1731362731422-phoenix_cropped.png"
-                alt = "Shared Media"
-            />
-
-            <img 
-                className="SharedPhoto-image"
-                src = "https://storage.googleapis.com/chatez-438923.firebasestorage.app/ProfilePhotos%2F56rolsj%40gmail.com-1731363038271-tenor.gif" 
-                alt = "Shared Media"
-            />
+            {sharedImages.map((image, index) => (
+                <img 
+                    key={index}
+                    className="SharedImage"
+                    src = {image}
+                    alt = "Shared Media"
+                />
+            ))}
 
             <h2>Shared Files</h2>
 
-            <p className="Document">DocumentExample1.docx</p>
-            <p className="Document">DocumentExample2.pdf</p>
+            {sharedFiles.map((file, index) => (
+                <p 
+                    key = {index}
+                    className="Document"
+                >
+                    {file}
+                </p>
+            ))}
 
             <button
                 id = "DeleteConversation-button"

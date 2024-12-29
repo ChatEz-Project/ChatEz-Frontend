4d9c4a688f5c79afc1c5eb96d7882d5ad4eb4170
Abdul Rehman <87334764+X2Abdul@users.noreply.github.com>
2024-12-29 23:02:54 +0000

    Added setDisplayName and setLanguage implementation

--- chatez/src/pages/Settings/Settings.tsx
+++ chatez/src/pages/Settings/Settings.tsx
@@ -1,71 +1,22 @@
-import React, {Fragment, useEffect, useRef, useState} from "react";
-import {useAuth} from "../../contexts/authContext";
+import React, { useEffect, useRef, useState } from 'react';
+import { useAuth } from '../../contexts/authContext';
 import Logo from '../../media/logo.svg';
 import '../../global.css';
-import {useHistory} from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './Settings.css'
-import {deleteAllUserConversations, deleteUser, getUser, setProfilePhoto} from "../../backend/endpoints";
-import {User} from "../../backend/types";
-import {doSignOut} from "../../firebase/auth";
-import {cleanupCache} from "../Chats/Components/ChatFeedFriendPanel/ChatFeedFriendPanel";
-
-// Define the language options with ISO-639 codes
-const languageOptions = [
-  { code: 'af', name: 'Afrikaans' },
-  { code: 'ar', name: 'Arabic' },
-  { code: 'bn', name: 'Bengali' },
-  { code: 'bg', name: 'Bulgarian' },
-  { code: 'ca', name: 'Catalan' },
-  { code: 'zh', name: 'Chinese' },
-  { code: 'hr', name: 'Croatian' },
-  { code: 'cs', name: 'Czech' },
-  { code: 'da', name: 'Danish' },
-  { code: 'nl', name: 'Dutch' },
-  { code: 'en', name: 'English' },
-  { code: 'et', name: 'Estonian' },
-  { code: 'tl', name: 'Filipino' },
-  { code: 'fi', name: 'Finnish' },
-  { code: 'fr', name: 'French' },
-  { code: 'de', name: 'German' },
-  { code: 'el', name: 'Greek' },
-  { code: 'gu', name: 'Gujarati' },
-  { code: 'iw', name: 'Hebrew' },
-  { code: 'hi', name: 'Hindi' },
-  { code: 'hu', name: 'Hungarian' },
-  { code: 'is', name: 'Icelandic' },
-  { code: 'id', name: 'Indonesian' },
-  { code: 'ga', name: 'Irish' },
-  { code: 'it', name: 'Italian' },
-  { code: 'ja', name: 'Japanese' },
-  { code: 'kn', name: 'Kannada' },
-  { code: 'ko', name: 'Korean' },
-  { code: 'lv', name: 'Latvian' },
-  { code: 'lt', name: 'Lithuanian' },
-  { code: 'ms', name: 'Malay' },
-  { code: 'ml', name: 'Malayalam' },
-  { code: 'mr', name: 'Marathi' },
-  { code: 'no', name: 'Norwegian' },
-  { code: 'fa', name: 'Persian' },
-  { code: 'pl', name: 'Polish' },
-  { code: 'pt', name: 'Portuguese' },
-  { code: 'ro', name: 'Romanian' },
-  { code: 'ru', name: 'Russian' },
-  { code: 'sr', name: 'Serbian' },
-  { code: 'sk', name: 'Slovak' },
-  { code: 'sl', name: 'Slovenian' },
-  { code: 'es', name: 'Spanish' },
-  { code: 'sw', name: 'Swahili' },
-  { code: 'sv', name: 'Swedish' },
-  { code: 'ta', name: 'Tamil' },
-  { code: 'te', name: 'Telugu' },
-  { code: 'th', name: 'Thai' },
-  { code: 'tr', name: 'Turkish' },
-  { code: 'uk', name: 'Ukrainian' },
-  { code: 'ur', name: 'Urdu' },
-  { code: 'vi', name: 'Vietnamese' },
-  { code: 'cy', name: 'Welsh' },
-];
+import './Settings.css';
+import {
+  deleteAllUserConversations,
+  deleteUser,
+  getUser,
+  setDisplayName,
+  setLanguage,
+  setProfilePhoto,
+} from '../../backend/endpoints';
+import { User } from '../../backend/types';
+import { doSignOut } from '../../firebase/auth';
+import { cleanupCache } from '../Chats/Components/ChatFeedFriendPanel/ChatFeedFriendPanel';
+import { languageOptions } from './Language';
 
 const Settings: React.FC = () => {
   const { currentUser, currentUserAccessToken } = useAuth();
@@ -73,28 +24,38 @@ const Settings: React.FC = () => {
   const [backendUser, setBackendUser] = useState<User | null>(null);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null);
+  const [displayNameValue, setDisplayNameValue] = useState(
+    backendUser?.displayName || ''
+  );
+  const [dn, setDn] = useState(backendUser?.displayName);
 
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleProfilePicChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleProfilePicChange = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const file = e.target.files?.[e.target.files.length - 1];
     if (file) {
       try {
         await setProfilePhoto(currentUserAccessToken, file);
-        setBackendUser(await getUser(currentUserAccessToken || "", currentUser?.email || ""));
+        setBackendUser(
+          await getUser(currentUserAccessToken || '', currentUser?.email || '')
+        );
 
-        const profilePicElement = document.getElementById('ProfilePic-icon') as HTMLImageElement;
-        profilePicElement.src = `${backendUser?.photoUrl || ""}`;
+        const profilePicElement = document.getElementById(
+          'ProfilePic-icon'
+        ) as HTMLImageElement;
+        profilePicElement.src = `${backendUser?.photoUrl || ''}`;
 
-        cleanupCache()
-      } catch(err) {
+        cleanupCache();
+      } catch (err) {
         console.error(err);
       }
     }
   };
 
   const getLanguageName = (code: string): string => {
-    const language = languageOptions.find(lang => lang.code === code);
+    const language = languageOptions.find((lang) => lang.code === code);
     return language ? language.name : code;
   };
 
@@ -111,7 +72,8 @@ const Settings: React.FC = () => {
       try {
         const user = await getUser(currentUserAccessToken, currentUser.email);
         setBackendUser(user);
-        setSelectedLanguage(user.language || "");
+        setSelectedLanguage(user.language || '');
+        setDn(user.displayName);
       } catch (error) {
         console.error('Failed to load user data:', error);
       }
@@ -134,53 +96,70 @@ const Settings: React.FC = () => {
     };
   }, []);
 
-  const handleLanguageChange = (languageCode: string) => {
+  const handleLanguageChange = async (languageCode: string) => {
     setSelectedLanguage(languageCode);
     setIsDropdownOpen(false);
-    console.log("save new language:", languageCode);
+  };
+
+  const handleLanguageSave = async (languageCode: string | null) => {
+    try {
+      if (languageCode) {
+        await setLanguage(currentUserAccessToken, languageCode);
+      }
+    } catch (err) {
+      console.error('Error saving language: ', err);
+    }
+  };
+
+  const handleDisplayNameSave = async (displayName: string) => {
+    try {
+      if (displayName) {
+        await setDisplayName(currentUserAccessToken, displayName);
+        setDn(displayName);
+      }
+    } catch (err) {
+      console.error('Error saving display name: ', err);
+    }
   };
 
   return (
-    <div className="Settings vh-100"
-         style={{
-           backgroundColor: "var(--background-color2)",
-           color: '#000000',
-           fontSize: '40px'
-         }}>
+    <div
+      className="Settings vh-100"
+      style={{
+        backgroundColor: 'var(--background-color2)',
+        color: '#000000',
+        fontSize: '40px',
+      }}
+    >
       <div className="row">
         <div className="col-sm-1">
           <button
             className="btn m-4 fs-1 d-flex justify-content-start"
-            onClick={() => history.push("/chats")}
+            onClick={() => history.push('/chats')}
           >
             <i className="bi bi-arrow-left"></i>
           </button>
         </div>
         <div className="col justify-items-center pt-4">
-          <img
-            src={Logo}
-            alt="Logo"
-            style={{width: '200px'}}
-          />
+          <img src={Logo} alt="Logo" style={{ width: '200px' }} />
         </div>
         <div className="col-sm-1"></div>
       </div>
       <div className="row">
         <div className="col-md"></div>
-        <div className="col-md w-25"
-             style={{borderRight: '6px solid white'}}>
-          <div style={{position: 'relative', display: 'inline-block'}}>
+        <div className="col-md w-25" style={{ borderRight: '6px solid white' }}>
+          <div style={{ position: 'relative', display: 'inline-block' }}>
             <img
               className="row"
               id="ProfilePic-icon"
               src={backendUser?.photoUrl || undefined}
               alt="Profile icon"
-              style={{width: '200px', height: '200px'}}
+              style={{ width: '200px', height: '200px' }}
             />
             <input
               type="file"
               ref={fileInputRef}
-              style={{display: 'none'}}
+              style={{ display: 'none' }}
               onChange={handleProfilePicChange}
             />
             <button
@@ -188,25 +167,27 @@ const Settings: React.FC = () => {
               onClick={() => attachFile()}
               style={{
                 bottom: '-20px',
-                right: '-30px'
+                right: '-30px',
               }}
             >
-              <i className="bi bi-camera display-3"
-                 style={{
-                   fontSize: '60px',
-                   color: '#43A656'
-                 }}>
-              </i>
+              <i
+                className="bi bi-camera display-3"
+                style={{
+                  fontSize: '60px',
+                  color: '#43A656',
+                }}
+              ></i>
             </button>
           </div>
-          <h1 className="row d-flex justify-content-md-center">
-            {backendUser?.displayName}
-          </h1>
-          <h1 className="row d-flex justify-content-center" style={{fontSize: '26px'}}>
+          <h1 className="row d-flex justify-content-md-center">{dn}</h1>
+          <h1
+            className="row d-flex justify-content-center"
+            style={{ fontSize: '26px' }}
+          >
             {currentUser?.email}
           </h1>
         </div>
-        <div className="col-md w-25 flex-column d-flex align-items-start" >
+        <div className="col-md w-25 flex-column d-flex align-items-start">
           <h1>Settings</h1>
           <div className="mt-5">
             <h1 className="input-header">Change Name:</h1>
@@ -214,11 +195,13 @@ const Settings: React.FC = () => {
               <input
                 className="form-control"
                 placeholder={backendUser?.displayName || undefined}
+                value={displayNameValue}
+                onChange={(e) => setDisplayNameValue(e.target.value)}
               />
               <button
                 className="btn submit-input m-1 p-0 fs-3"
-                onClick={() => console.log("save new displayName")}
-                style={{width: '50px'}}
+                onClick={() => handleDisplayNameSave(displayNameValue)}
+                style={{ width: '50px' }}
                 id="Submit-name-change"
               >
                 <i className="bi bi-arrow-up-right"></i>
@@ -230,7 +213,7 @@ const Settings: React.FC = () => {
             <div
               className="dropdown d-flex align-items-center"
               id="language-dropdown-container"
-              style={{position: 'relative'}}
+              style={{ position: 'relative' }}
             >
               <button
                 className="btn dropdown-toggle"
@@ -240,10 +223,12 @@ const Settings: React.FC = () => {
                   backgroundColor: 'white',
                   border: '1px solid #ced4da',
                   minWidth: '250px',
-                  textAlign: 'left'
+                  textAlign: 'left',
                 }}
               >
-                {selectedLanguage ? getLanguageName(selectedLanguage) : 'Select Language'}
+                {selectedLanguage
+                  ? getLanguageName(selectedLanguage)
+                  : 'Select Language'}
               </button>
               {isDropdownOpen && (
                 <div
@@ -253,13 +238,15 @@ const Settings: React.FC = () => {
                     width: '100%',
                     maxHeight: '300px',
                     overflowY: 'auto',
-                    zIndex: 1000
+                    zIndex: 1000,
                   }}
                 >
                   {languageOptions.map((language) => (
                     <button
                       key={language.code}
-                      className={`dropdown-item ${selectedLanguage === language.code ? 'active' : ''}`}
+                      className={`dropdown-item ${
+                        selectedLanguage === language.code ? 'active' : ''
+                      }`}
                       onClick={() => handleLanguageChange(language.code)}
                     >
                       {language.name}
@@ -269,8 +256,8 @@ const Settings: React.FC = () => {
               )}
               <button
                 className="btn submit-input m-1 p-0 fs-3"
-                onClick={() => console.log("save new language selection")}
-                style={{width: '40px'}}
+                onClick={() => handleLanguageSave(selectedLanguage)}
+                style={{ width: '40px' }}
                 id="Submit-language-change"
               >
                 <i className="bi bi-arrow-up-right"></i>
@@ -287,7 +274,7 @@ const Settings: React.FC = () => {
                   console.error('Error deleting conversations:', error);
                 }
               }}
-              style={{width: '300px'}}
+              style={{ width: '300px' }}
               id="Delete-all-messages"
             >
               Delete All Messages
@@ -306,7 +293,7 @@ const Settings: React.FC = () => {
                   console.error('Error deleting user:', error);
                 }
               }}
-              style={{width: '300px'}}
+              style={{ width: '300px' }}
               id="Delete-account"
             >
               Delete Account
@@ -314,7 +301,6 @@ const Settings: React.FC = () => {
           </div>
         </div>
         <div className="col-md"></div>
-
       </div>
     </div>
   );
